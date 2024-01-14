08d0dcd46281c98897a23b980db5ac48bb7c2a24
Dongtaes <ardanazimatalay@gmail.com>
2024-01-14 22:11:25 +0100

    Initial Commit

---
+++ EmojiApp/EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppApp.swift
+//  EmojiApp
+//
+//  Created by Dongtaes on 14.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
