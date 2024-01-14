fd5f77d8fcab13c5f32c31af87f6ac3738f65d71
Dongtaes <ardanazimatalay@gmail.com>
2024-01-14 22:35:57 +0100

    V0.1

--- EmojiApp/ContentView.swift
+++ EmojiApp/ContentView.swift
@@ -7,15 +7,32 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case rocket = "🚀"
+    case cross = "❌"
+    case wrench = "🔧"
+    case sleepy = "💤"
+}
+
 struct ContentView: View {
+    @State var selection = Emoji.rocket
+ 
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                Text((selection.rawValue))
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) {emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .padding()
+            .navigationTitle("First App!")
         }
-        .padding()
     }
 }
 
